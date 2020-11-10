c2c85bade65525e8f09e79fe82d530d2ee3b00e3
Jebitok <sharonkosgei4@gmail.com>
2020-11-10 22:55:38 +0300

    global variable & modules & require basics on nodejs

---
+++ crash-cource/modules.js
@@ -0,0 +1,3 @@
+const xyz = require('./playground/people');
+
+console.log(xyz)
---
+++ crash-cource/playground/people.js
@@ -0,0 +1,6 @@
+//modules & require
+
+const people = ["Edna", "Eliazer", "Sharon", "Steve", "Ed", "Ruto", "Jay"]
+// console.log(people);
+
+module.exports = people;

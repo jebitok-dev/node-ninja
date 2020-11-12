5d790d5daf7192cf6872c7014d40cf4bc2abe2d3
Jebitok <sharonkosgei4@gmail.com>
2020-11-12 20:27:31 +0300

    linking with external HTML file

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method) //run localhost:3000 on browser
 
     res.setHeader('Content-type', 'text/html');
 
-    res.write('<head><link rel="stylesheet" href="#"></head>');
-    res.write('<p>hello ninjas</p>');
-    res.write('<p>hello again ninjas</p>');
+    fs.readFile('./views/index.html', (err, data) => {
+        if(err) {
+            console.log(err) //keeps request hanging
+        } else {
+            res.write(data);
+            res.end();
+        }
+    })
+    
 
-    res.end();
 })
 server.listen(3000, 'localhost', () => {
     console.log('Listening for request on port 3000')

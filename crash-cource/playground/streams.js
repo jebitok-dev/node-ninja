771a21b0624bbfb3c0f103825580367a0670b1bb
Jebitok <sharonkosgei4@gmail.com>
2020-11-12 05:28:57 +0300

    write stream

--- crash-cource/playground/streams.js
+++ crash-cource/playground/streams.js
@@ -2,10 +2,12 @@
 // streams --> start using data, before it has finished 
 
 const fs = require('fs');
-const readStream = fs.createReadStream('././docs/blog3.txt')
-
+const readStream = fs.createReadStream('././docs/blog3.txt', {encoding: 'utf-8'});
+const writeStream = fs.createWriteStream('././docs/blog4.txt');
 //event listener 
 readStream.on('data', (chunk) => {
     console.log('.....NEW CHUNK.....')
-    console.log(chunk.toString())
+    console.log(chunk)
+    writeStream.write('\nNEW CHUNK\n');
+    writeStream.write(chunk);
 } )

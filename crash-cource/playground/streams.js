a36d673aa7594bb5f1ad7ae179a4bcd7801c45b5
Jebitok <sharonkosgei4@gmail.com>
2020-11-12 05:32:25 +0300

    piping method

--- crash-cource/playground/streams.js
+++ crash-cource/playground/streams.js
@@ -5,9 +5,12 @@ const fs = require('fs');
 const readStream = fs.createReadStream('././docs/blog3.txt', {encoding: 'utf-8'});
 const writeStream = fs.createWriteStream('././docs/blog4.txt');
 //event listener 
-readStream.on('data', (chunk) => {
-    console.log('.....NEW CHUNK.....')
-    console.log(chunk)
-    writeStream.write('\nNEW CHUNK\n');
-    writeStream.write(chunk);
-} )
+// readStream.on('data', (chunk) => {
+//     console.log('.....NEW CHUNK.....')
+//     console.log(chunk)
+//     writeStream.write('\nNEW CHUNK\n');
+//     writeStream.write(chunk);
+// } )
+
+//piping ---> .pipe
+readStream.pipe(writeStream);

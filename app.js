98ed4a79e50e22a6c641dac298ff1eea99490e80
Jebitok <sharonkosgei4@gmail.com>
2020-11-16 21:00:39 +0300

    ejs files

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const express = require('express');
 //express app
 const app = express();
 
+//register view engine
+app.set("view engine", 'ejs');
+
 //listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    // res.send('<p>home page</p>');
-     //content type
+    res.render('index');
 
-     res.sendFile('./views/index.html', {root: __dirname});
+    //  res.sendFile('./views/index.html', {root: __dirname});
 });
 
 app.get('/about', (req, res) => {

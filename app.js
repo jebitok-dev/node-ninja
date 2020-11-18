e438c88c2cb1e1e3f4fab9b00851d0e29a37ad03
Jebitok <sharonkosgei4@gmail.com>
2020-11-18 23:45:28 +0300

    passing data through app's handler into view engine - ejs

--- app.js
+++ app.js
@@ -10,21 +10,23 @@ app.set("view engine", 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {title: 'Home'});
 
     //  res.sendFile('./views/index.html', {root: __dirname});
 });
 
 app.get('/about', (req, res) => {
     // res.send('<p>about page</p>'); //content type
-    res.sendFile('./views/about.html', {root: __dirname});
+    // res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', {title: 'About'})
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', {title: 'Create a new Blog'});
 })
 
 //404 -- every request not url (at the bottom positioning)
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname})
+    // res.status(404).sendFile('./views/404.html', { root: __dirname})
+    res.status(404).render('404', {title: '404'})
 })

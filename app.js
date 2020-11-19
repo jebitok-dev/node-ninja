9973067bbbc9d40dcce4b079b7aeb7efec64a227
Jebitok <sharonkosgei4@gmail.com>
2020-11-19 12:03:09 +0300

    using third party middleware - morgan

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 //express app
 const app = express();
@@ -9,18 +10,7 @@ app.set("view engine", 'ejs');
 //listen for requests
 app.listen(3000);
 
-app.use((req, res, next) => {
-    console.log('new request made:');
-    console.log('host: ', req.hostname);
-    console.log('path: ', req.path);
-    console.log('method: ', req.method);
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log('in the next middleware');
-    next();
-});
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     const blogs = [
